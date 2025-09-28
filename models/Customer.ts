2a9d25b760053e6eff228f571e46cef6fb435a98
Jawwad-codes <jawwadnadeem78@gmail.com>
2025-09-28 17:26:50 +0500

    customer not done yet

--- models/Customer.ts
+++ models/Customer.ts
@@ -1,9 +1,11 @@
-import mongoose, { Schema, model, models } from "mongoose"
+/** @format */
+
+import mongoose, { Schema, model, models } from "mongoose";
 
 const CustomerSchema = new Schema({
   email: { type: String, required: true, unique: true },
   userOrderId: { type: String, required: true }, // permanent tracking id
   createdAt: { type: Date, default: Date.now },
-})
+});
 
-export default models.Customer || model("Customer", CustomerSchema)
+export default models.Customer || model("Customer", CustomerSchema);
